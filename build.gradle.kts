4828b61e0c041949a0a7a1185689d726ba77fbb9
Kartoffelsup <kartoffelsup@users.noreply.github.com>
2018-12-05 23:34:04 +0100

    Add maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   id("org.jetbrains.kotlin.jvm").version("1.3.10")
+  id("maven-publish")
 }
 
 // If https://github.com/querydsl/querydsl/pull/2388 gets accepted
@@ -8,6 +9,7 @@ val queryDslVersion = "4.2.1"
 val h2Version = "1.4.197"
 val logbackVersion = "1.2.3"
 
+group = "io.github.kartoffelsup"
 version = "0.0.1-SNAPSHOT"
 
 repositories {
@@ -15,8 +17,22 @@ repositories {
   mavenCentral()
 }
 
+publishing {
+  repositories {
+    mavenLocal()
+  }
+  publications {
+    create<MavenPublication>("maven") {
+      artifactId = rootProject.name
+      println(components)
+      from(components["java"])
+    }
+  }
+}
+
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
   compile("com.querydsl:querydsl-sql-codegen:$queryDslCodegenVersion")
   compile("com.querydsl:querydsl-sql:$queryDslVersion")
 
@@ -24,6 +40,7 @@ dependencies {
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
   testImplementation("com.h2database:h2:$h2Version")
   testImplementation("ch.qos.logback:logback-classic:$logbackVersion")
-  // Adding a dependency on the compiler breaks querydsl due to a new guava version
-//  testImplementation("org.jetbrains.kotlin:kotlin-compiler")
+  // adding a dependency on kotlin-compiler introduces an old guava version bundled in the kotlin compiler
+  // a which breaks querydsl
+  // testImplementation("org.jetbrains.kotlin:kotlin-compiler")
 }
