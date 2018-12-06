ed322df9210380fc9e92ccb95c318c7576f48d2c
Kartoffelsup <kartoffelsup@users.noreply.github.com>
2018-12-06 20:46:48 +0100

    Introduce extension function to convert types to simple kotlin Types

--- src/main/kotlin/io/github/kartoffelsup/ktqdsl/JavaToKotlinTypeTransformer.kt
+++ src/main/kotlin/io/github/kartoffelsup/ktqdsl/JavaToKotlinTypeTransformer.kt
@@ -3,84 +3,31 @@ package io.github.kartoffelsup.ktqdsl
 import com.mysema.codegen.model.SimpleType
 import com.mysema.codegen.model.Type
 
+private fun Type.asSimpleKotlinType(kotlinName: String): Type {
+  return SimpleType(
+    category,
+    "kotlin.$kotlinName",
+    "kotlin",
+    kotlinName,
+    isPrimitive,
+    isFinal
+  )
+}
+
 object JavaToKotlinTypeTransformer {
   fun transform(type: Type): Type {
     val simpleName = type.simpleName
     // TODO kartoffelsup: What about arrayType, componentType, enclosingType, parameters?
-    // TODO kartoffelsup: Generalize
     return when (simpleName) {
-      "Integer", "int" -> SimpleType(
-        type.category,
-        "kotlin.Int",
-        "kotlin",
-        "Int",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "Short", "short" -> SimpleType(
-        type.category,
-        "kotlin.Short",
-        "kotlin",
-        "Short",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "Float", "float" -> SimpleType(
-        type.category,
-        "kotlin.Float",
-        "kotlin",
-        "Float",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "Double", "double" -> SimpleType(
-        type.category,
-        "kotlin.Double",
-        "kotlin",
-        "Double",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "Byte", "byte" -> SimpleType(
-        type.category,
-        "kotlin.Byte",
-        "kotlin",
-        "Byte",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "Long", "long" -> SimpleType(
-        type.category,
-        "kotlin.Long",
-        "kotlin",
-        "Long",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "Boolean", "boolean" -> SimpleType(
-        type.category,
-        "kotlin.Boolean",
-        "kotlin",
-        "Boolean",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "String" -> SimpleType(
-        type.category,
-        "kotlin.String",
-        "kotlin",
-        "String",
-        type.isPrimitive,
-        type.isFinal
-      )
-      "byte[]", "Byte[]" -> SimpleType(
-        type.category,
-        "kotlin.ByteArray",
-        "kotlin",
-        "ByteArray",
-        type.isPrimitive,
-        type.isFinal
-      )
+      "Integer", "int" -> type.asSimpleKotlinType("Int")
+      "Short", "short" -> type.asSimpleKotlinType("Short")
+      "Float", "float" -> type.asSimpleKotlinType("Float")
+      "Double", "double" -> type.asSimpleKotlinType("Double")
+      "Byte", "byte" -> type.asSimpleKotlinType("Byte")
+      "Long", "long" -> type.asSimpleKotlinType("Long")
+      "Boolean", "boolean" -> type.asSimpleKotlinType("Boolean")
+      "String" -> type.asSimpleKotlinType("String")
+      "byte[]", "Byte[]" -> type.asSimpleKotlinType("ByteArray")
       else -> type
     }
   }
